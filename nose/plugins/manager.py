d8e6f8789afe11f755a5906b9d9860923f52ea92
Claudio Figueiredo <jcfigueiredo@gmail.com>
2013-10-07 11:37:24 -0400

    avoiding aborting due invalid plugins

--- nose/plugins/manager.py
+++ nose/plugins/manager.py
@@ -329,6 +329,8 @@ class ZeroNinePlugin:
             if not hasattr(self.plugin, 'addDeprecated'):
                 return
             return self.plugin.addDeprecated(test.test)
+        if test is None:
+            return
         # add capt
         capt = test.capturedOutput
         return self.plugin.addError(test.test, err, capt)
